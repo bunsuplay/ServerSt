19897f193c36a9f3eaee27241e4fda40219e60eb
seungwon <>
2022-04-30 10:35:41 +0900

    Update Messege.h

--- test/Messege.h
+++ test/Messege.h
@@ -1,12 +1,12 @@
 
-public enum class MessageType
-{
-	LogIn,
-	LogOut,
-	chat,
-
-	Length,// 제가 가진 메시지 타입의 개수보다 더 많은 내요잉 들어오면 무시!
-};
+//enum class MessageType
+//{
+//	LogIn,
+//	LogOut,
+//	chat,
+//
+//	Length,// 제가 가진 메시지 타입의 개수보다 더 많은 내요잉 들어오면 무시!
+//};
 
 
 
