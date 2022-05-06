5b4b3fd27cc9a6a53a3a8289570d783e8218ff42
seungwon <>
2022-05-07 08:55:42 +0900

    Update Messageinfo.h

--- test/Messageinfo.h
+++ test/Messageinfo.h
@@ -1,84 +1,68 @@
-// 통합! 이라고 하는 것이죠!\
-// Struct랑 비슷하게 쓰실 수 있습니다.
-// 차이점이 무었이냐? 하나의 메모리를 여러가지 자료형이 공유해요!
+//통합! 이라고 하는 것이죠!
+//Struct랑 비슷하게 쓰실 수 있습니다!
+//차이점이 무엇이냐? 하나의 메모리를 여러가지 자료형이 공유해요!
 
 //[0][0][0][10]
-//			10 int
-//			\n	char[i]
-
+//          10  int
+//          \n  char[3]
 union ConvertionBase
 {
 	unsigned int uInteger;
 	int integer;
-
 	float floating;
-
 	char character[4];
-
 	short shortInteger[2];
-	unsigned short ushortInteger[2];
+	unsigned short uShortInteger[2];
 };
-
 ConvertionBase byteConvertor;
 
-//항상 클라이언트의 enum 값과 일치시켜주세요!
+//항상 클라이언트의 enum값과 일치시켜주세요!
 enum class MessageType
 {
-	EndOfLine, // 문자열의 끝을 판별할 때 '\0'이라고 하는 걸 파악했습니다. 메시지가 뒤에 더없다 라고 하는 걸 endofline으로 표시!
+	EndOfLine, //문자열의 끝을 판단할 때 \0이라고 하는 걸 파악했습니다! 메시지가 뒤에 더 없다 라고 하는 걸 EndOfLine으로 표시!
 	LogIn,
 	LogOut,
-	chat,
+	Chat,
 
-
-	Unknown, // 서버 전용 모르겠다! 선언!
-
-	Length,// 제가 가진 메시지 타입의 개수보다 더 많은 내용 들어오면 무시!
+	Unknown,  //서버 전용 모르겠다 선언!
+	Length
+	//제가 가진 메시지 타입의 개수보다 더 많은 내용이 들어오면 무시!
 };
-
 
 class MessageInfo
 {
-public :
+public:
 	MessageType type;
 	int length;
 };
 
-class MessageInfo_LogIn : public  MessageInfo
+class MessageInfo_Login : public MessageInfo
 {
-public :
+public:
 	int userIndex;
 	string name;
-public:
-	MessageInfo_LogIn(char* message, int targetUser)
+	MessageInfo_Login(char* message, int targetUser)
 	{
 		type = MessageType::LogIn;
-		//길이 부분의 두 개 확인!
-		//byteConvertor.character[0] = message[2];
-		//byteConvertor.character[1] = message[3];
-		//캐릭터 두 개를 읽어와서 그거를 숫자로 취급!
-		//length = byteConvertor.shortInteger[0];
+
 		userIndex = targetUser;
-		// 메시지의 4번째부터 내용을 넣어주도록 합시다!
+		//메시지의 4번째부터 내용을 넣어주도록 합시다!
 		name = &(message[4]);
 	}
 };
 
-class MessageInfo_Chat : public  MessageInfo
+class MessageInfo_Chat : public MessageInfo
 {
 public:
 	int userIndex;
 	string value;
-public:
+
 	MessageInfo_Chat(char* message, int targetUser)
 	{
-		type = MessageType::chat;
-		//길이 부분의 두 개 확인!
-		//byteConvertor.character[0] = message[2];
-		//byteConvertor.character[1] = message[3];
-		//캐릭터 두 개를 읽어와서 그거를 숫자로 취급!
-		//length = byteConvertor.shortInteger[0];
+		type = MessageType::Chat;
+
 		userIndex = targetUser;
-		// 메시지의 4번째부터 내용을 넣어주도록 합시다!
+		//메시지의 4번째부터 내용을 넣어주도록 합시다!
 		value = &(message[4]);
 	}
 };
