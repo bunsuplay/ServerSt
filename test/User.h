d2af8a1dcd2655420901816499445cde78811bba
seungwon <>
2022-05-07 11:33:42 +0900

    Update User.h

--- test/User.h
+++ test/User.h
@@ -53,7 +53,7 @@ public:
 		{
 			//write는요 데이터를 보냈을 때 보낸 길이를 다시 저희한테 알려줍니다!
 			// 0보다 크다고 한다면 메시지가 잘 보내졌다고 볼 수 있지 않을까요?
-			if (write(sendFD, message, length))
+			if (write(fdNumber, waitSend,waitLength))
 			{
 				// 보낼 내용도 0으로 추가!
 				memset(waitSend, 0, waitLength);
