933d7fbdf5d5ee4afdc6eaa0b77b281b421df848
seungwon <>
2022-05-08 09:20:24 +0900

    Update sever.cpp

--- test/sever.cpp
+++ test/sever.cpp
@@ -86,7 +86,6 @@ void* SendThread(void* data)
 	int checkNumber;
 	while (true)
 	{
-		cout << "Looping...." << endl;
 		checkNumber = 0;
 		// 유저 전에 돌아주기!
 		for (int i = 1; i < MAX_USER_NUMBER; i++)
