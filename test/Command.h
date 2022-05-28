ad8b66b49244f48ccf5d03cf1cbeb527dc8f30b9
seungwon <>
2022-05-29 08:56:02 +0900

    Update Command.h

--- test/Command.h
+++ test/Command.h
@@ -1,21 +1,22 @@
 #pragma once
 
- enum CommandType
+ enum class CommandType
 {
 	Error,
 	Quit,
 
 
 };
- // 현제 계산중인 
+ // 현제 계산중인 명령어의 종류
  CommandType currentType;
  // 현제 몇페이지 까지 읽었는가?
  int currentIndex;
 
  void CalculateCommand(string word)
  {
-	 
-
+	 cout << word << endl;
+	 //아직 아무것도 안 받았어요!
+	//그 말은 이제부터 저희가 무슨 명령어를 쓸 것인지 선택할 시간이라는 것입니다!
 	 if (currentIndex == 0)
 	 {
 		 if (word == "QUIT" || word == "EXIT" || word == "END" || word == "CLOSE")
@@ -27,7 +28,7 @@
 	 ++currentIndex;
  }
 
- void ExitcutComaand()
+ void ExecuteCommand()
  {
 	 switch (currentType)
 	 {
@@ -38,7 +39,7 @@
 		 break;
 	 default:
 		 break;
-	 }
+	 };
  }
 
 void* CommandThread(void* data)
@@ -49,7 +50,7 @@ void* CommandThread(void* data)
 		// 관리자의 입력을 받음
 		cin >> input;
 
-		// 현제 계산중인 
+		// 명령어를 초기화 하고 갈께요!
 		 currentType = CommandType::Error ;
 		// 아직 받은 것이 없으니까 0번째!
 		 currentIndex = 0;
@@ -69,7 +70,7 @@ void* CommandThread(void* data)
 		currentWord.clear();
 		for (int i = 0; i <= input.size(); i++)
 		{
-			//글자가 끝나거나 스페이스가 나오면 그자리에서 끊고 , 그자리에서 알아보기!
+			//글자가 끝나거나, 스페이스가 나오면 그 자리에서 끊고, 글자를 읽어보기!
 			if (i = input.size() ||  input[i] == ' ')
 			{
 				CalculateCommand(currentWord);
@@ -80,8 +81,8 @@ void* CommandThread(void* data)
 				currentWord += input[i];
 			};
 		};
-		//글자를 전부 파악했으니 실행
-		ExitcutComaand();
+		//글자를 모두 파악했으니 실제로 커맨드를 실행합니다!
+		ExecuteCommand();
 	};
 
 	return nullptr;
